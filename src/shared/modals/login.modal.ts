72283379528a413710cf6624016e640857b0f97d
Spencer Hehl <schehl49@gmail.com>
2017-07-22 15:39:37 -0400

    social integration fixes. only missing. local storage piece

--- src/shared/modals/login.modal.ts
+++ src/shared/modals/login.modal.ts
@@ -8,20 +8,31 @@ import { AuthService } from '../services/auth.service';
     templateUrl: 'login.modal.html'
 })
 export class LoginModal {
-    currUser: any;
     constructor(private authService: AuthService, private NavParams: NavParams, public viewController: ViewController, public googleAuth: GoogleAuth, public user: User, public facebookAuth: FacebookAuth){}
 
     facebookLogin(){
         this.facebookAuth.login().then((res) => {
-            this.authService.currentUser = this.user.social.facebook.data;
-            this.viewController.dismiss();
+            if(res.signup){
+                this.authService.newUser(this.user.social.facebook.data).subscribe(
+                    () => this.viewController.dismiss()
+                );
+            }else{
+                this.authService.login(this.user.social.facebook.data);
+                this.viewController.dismiss();
+            }
         });
     }
 
     googleLogin(){
         this.googleAuth.login().then((res) =>{
-            this.authService.currentUser = this.user.social.google.data;
-            this.viewController.dismiss();
+            if(res.signup){
+                this.authService.newUser(this.user.social.google.data).subscribe(
+                    () => this.viewController.dismiss()
+                );
+            }else{
+                this.authService.login(this.user.social.google.data);
+                this.viewController.dismiss();
+            }
         });
     }
 
--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@angular/core'
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { User } from '@ionic/cloud-angular';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class AuthService {
     currentUser: any;
-    facebookLogin(){
-        this.currentUser = {
-            firstName: "Spencer",
-            lastName: "Hehl"
-        }
+
+    constructor(public user: User, private http: Http){}
+
+    login(user){
+        this.currentUser = user;
+        this.currentUser.ionic_id = this.user.id;
     }
 
-    googleLogin(){
-        this.currentUser = {
-            firstName: "Spencer",
-            lastName: "Hehl"
-        }
+    newUser(user){
+        this.login(user);
+        let headers = new Headers({'Content-type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+        return this.http.post('/user/newuser', this.currentUser, options).map((response: Response) => {
+            return response.json();
+        }).catch(this.handleError);
     }
 
     isAuthenticated(){
         return !!this.currentUser;
+    }
+
+    private handleError(error: Response){
+        return Observable.throw(error.statusText);
     }
 }
