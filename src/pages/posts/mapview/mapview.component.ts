5ccd3ea532b43e34044533b59f0c442f5f97f7fa
Spencer Hehl <schehl49@gmail.com>
2017-08-23 17:01:16 -0400

    marker click functionality

--- src/pages/posts/mapview/mapview.component.ts
+++ src/pages/posts/mapview/mapview.component.ts
@@ -18,6 +18,7 @@ export class MapViewPage{
     myLocation: any;
     markers: any;
     map: any;
+    Post: any;
 
     constructor(public navCtrl: NavController, private navParams: NavParams,
         public alertCtrl: AlertController, private postService: PostService){}
@@ -59,9 +60,51 @@ export class MapViewPage{
                         lat: post.geolocation[1],
                         lng: post.geolocation[0]
                     }
-                    return new google.maps.Marker({
+                    var marker = new google.maps.Marker({
                         position: postLocation,
                         map: this.map,
+                        post: post
+                    })
+                    var contentString = '<ion-card [hidden]="Post.isFlagged">' +
+                    '<ion-item>' + 
+                      '<ion-avatar item-start>' + 
+                        '<img [src]="Post.poster?.profilePicture" (click)="viewProfile()">' + 
+                      '</ion-avatar>' + 
+                      '<h2 (click)="viewProfile()" style="font-weight:bold">{{Post.poster?.fullName}}</h2>' + 
+                      '<ion-note>' + 
+                          '{{Post.createdDate | dateAge}}' + 
+                      '</ion-note>' + 
+                      '<ion-buttons item-end>' + 
+                          '<button ion-button (click)="viewMore()" clear><ion-icon name="more"></ion-icon></button>' + 
+                      '</ion-buttons>' + 
+                    '</ion-item>'
+                    '<ion-card-content style="margin-top:1%">' + 
+                      '<h2 style="font-weight:bold">{{Post.content?.title}} <ion-icon *ngIf="Post.content?.location" name="at" color="primary"></ion-icon> {{Post.content?.location}}</h2>'        
+                      '<img *ngIf="Post.content.contentType == \'photo\'" [src]="Post?.content.img">' + 
+                      '<p>{{Post.content.body}}</p>' + 
+                    '</ion-card-content>' + 
+                    '<ion-row>' + 
+                      '<ion-col>' + 
+                        '<button ion-button icon-lef clear small (click)="likePost(Post._id)" [color]="likeColor">' + 
+                          '<ion-icon name="thumbs-up"></ion-icon>"' +
+                         '<div>{{Post.numLikes}} Likes</div>"' + 
+                        '</button>' + 
+                      '</ion-col>' + 
+                      '<ion-col>' +
+                        '<button ion-button icon-left clear small (click)="viewComments(Post)" color="dark">' +
+                          '<ion-icon name="text"></ion-icon>' +
+                          '<div>{{Post.numComments}} Comments</div>' +
+                        '</button>' +
+                      '</ion-col>' +
+                    '</ion-row>' +
+                  '</ion-card>';
+                    var infoWindow = new google.maps.InfoWindow({
+                        content: contentString
+                    })
+                    var self = this;
+                    marker.addListener('click', function(){
+                        self.Post = marker.post;
+                        infoWindow.open(self.map, marker);
                     })
                 })
             },
